912cf3c80af7c2d21f1066729db20df3a5d2df12
cc-morning <90960002+cc-morning@users.noreply.github.com>
2022-04-09 23:47:37 +0800

    Optimize mysql insert data
    https://github.com/cc-morning/mysql_async/commit/fb2ca11475596e90e6bf9f8e949af4132632f920

--- src/to/mysql.rs
+++ src/to/mysql.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use futures::stream::FuturesUnordered;
+use bytes::Bytes;
 use log::info;
 use mysql_async::{prelude::Queryable, Conn, Opts, Pool};
 use serde::{Deserialize, Serialize};
@@ -12,13 +12,6 @@ pub struct MySQL {
     url: String,
     table: String,
     columns: Option<Vec<String>>,
-
-    #[serde(default = "default_batch")]
-    batch: usize,
-}
-
-fn default_batch() -> usize {
-    10000
 }
 
 impl MySQL {
@@ -40,62 +33,42 @@ impl MySQL {
         let columns_len = columns.len();
 
         let sql = {
-            let mut pre = format!("INSERT INTO `{}` (", self.table);
-            let mut suf = String::from("VALUES (");
+            let mut sql = format!("LOAD DATA LOCAL INFILE 'tmp' INTO TABLE `{}` FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\r\n' (", self.table);
 
             for (index, column) in columns.iter().enumerate() {
-                pre.push('`');
-                pre.push_str(column);
-                pre.push('`');
-                suf.push('?');
+                sql.push('`');
+                sql.push_str(column);
+                sql.push('`');
 
                 if index < columns_len - 1 {
-                    pre.push_str(", ");
-                    suf.push_str(", ");
+                    sql.push_str(", ");
                 }
             }
 
-            pre.push_str(") ");
-            suf.push(')');
+            sql.push(')');
 
-            format!("{}{}", pre, suf)
+            sql
         };
-        info!("sql : {}", sql);
+        info!("sql : {:#?}", sql);
 
-        let futures = FuturesUnordered::new();
-
+        let mut conn = pool.get_conn().await?;
         let mut reader = unsafe { Pin::new_unchecked(reader) };
 
-        'out: loop {
-            let mut rows = Vec::with_capacity(self.batch);
+        let (sender, recv) = tokio::sync::mpsc::unbounded_channel();
 
-            loop {
-                if let Some(row) = reader.next().await {
-                    rows.push(row);
+        let recv_task = tokio::spawn(async move {
+            conn.load_data(sql, recv).await.unwrap();
+        });
 
-                    if rows.len() == self.batch {
-                        let (mut conn, sql) = (pool.get_conn().await?, sql.clone());
-                        futures.push(tokio::spawn(async move {
-                            conn.exec_batch(sql, rows).await.unwrap();
-                        }));
+        while let Some(row) = reader.next().await {
+            let mut row_str = row.join(",");
+            row_str.push_str("\r\n");
 
-                        rows = Vec::with_capacity(self.batch);
-                    }
-                } else {
-                    let (mut conn, sql) = (pool.get_conn().await?, sql.clone());
-                    futures.push(tokio::spawn(async move {
-                        conn.exec_batch(sql, rows).await.unwrap();
-                    }));
-
-                    break 'out;
-                }
-            }
+            sender.send(Bytes::from(row_str)).unwrap();
         }
 
-        {
-            use futures::StreamExt;
-            let _ = futures.into_future().await;
-        }
+        drop(sender);
+        recv_task.await?;
 
         Ok(())
     }
